7d330217eb277ab62e162c6cb8515b1fdc5d7f6d
elixirprogrammer <anthony@elixirprogrammer.com>
2022-05-10 16:44:30 -0400

    adds api reference

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,18 +1,68 @@
 defmodule ExBanking do
   @moduledoc """
-  Documentation for `ExBanking`.
+  Public interface and behaviour for `ExBanking` operations.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExBanking.hello()
-      :world
-
+  Creates new user in the system
+  New user has zero balance of any currency
   """
-  def hello do
-    :world
+  @spec create_user(user :: String.t()) :: :ok | {:error, :wrong_arguments | :user_already_exists}
+  def create_user(user) do
+  end
+
+  @doc """
+  Increases user's balance in given currency by amount value
+  Returns new_balance of the user in given format
+  """
+  @spec deposit(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def deposit(user, amount, currency) do
+  end
+
+  @doc """
+  Decreases user's balance in given currency by amount value
+  Returns new_balance of the user in given format
+  """
+  @spec withdraw(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error,
+             :wrong_arguments
+             | :user_does_not_exist
+             | :not_enough_money
+             | :too_many_requests_to_user}
+  def withdraw(user, amount, currency) do
+  end
+
+  @doc """
+  Returns balance of the user in given format
+  """
+  @spec get_balance(user :: String.t(), currency :: String.t()) ::
+          {:ok, balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def get_balance(user, currency) do
+  end
+
+  @doc """
+  Decreases from_user's balance in given currency by amount value
+  Increases to_user's balance in given currency by amount value
+  Returns balance of from_user and to_user in given format
+  """
+  @spec send(
+          from_user :: String.t(),
+          to_user :: String.t(),
+          amount :: number,
+          currency :: String.t()
+        ) ::
+          {:ok, from_user_balance :: number, to_user_balance :: number}
+          | {:error,
+             :wrong_arguments
+             | :not_enough_money
+             | :sender_does_not_exist
+             | :receiver_does_not_exist
+             | :too_many_requests_to_sender
+             | :too_many_requests_to_receiver}
+  def send(from_user, to_user, amount, currency) do
   end
 end
